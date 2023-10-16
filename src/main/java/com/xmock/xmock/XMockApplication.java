231976341bc2c667d6726665090fd44e9d7c7477
kshitizsaini113 <kshitizsaini.rtk@gmail.com>
2023-10-16 23:05:18 +0530

    Inidial code generated by Spring Initializr

---
+++ src/main/java/com/xmock/xmock/XMockApplication.java
@@ -0,0 +1,13 @@
+package com.xmock.xmock;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class XMockApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(XMockApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/xmock/xmock/XMockApplicationTests.java
@@ -0,0 +1,13 @@
+package com.xmock.xmock;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class XMockApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
